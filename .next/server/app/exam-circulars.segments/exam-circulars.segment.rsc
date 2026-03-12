c5e592da8d60c6cdafa53f64e5673d8bce707b7b
Mohamed-Hashim-km <hashimhx3@gmail.com>
2026-03-12 09:56:37 +0530

    middlawar

--- .next/server/app/exam-circulars.segments/exam-circulars.segment.rsc
+++ .next/server/app/exam-circulars.segments/exam-circulars.segment.rsc
@@ -1,4 +1,4 @@
 1:"$Sreact.fragment"
 2:I[39756,["/_next/static/chunks/ff1a16fafef87110.js","/_next/static/chunks/247eb132b7f7b574.js"],"default"]
 3:I[37457,["/_next/static/chunks/ff1a16fafef87110.js","/_next/static/chunks/247eb132b7f7b574.js"],"default"]
-0:{"buildId":"5ccYR-Q1Ymm1V4Y28jZvO","rsc":["$","$1","c",{"children":[null,["$","$L2",null,{"parallelRouterKey":"children","template":["$","$L3",null,{}]}]]}],"loading":null,"isPartial":false}
+0:{"buildId":"x6E_hxZx2-DGXSOM1VF96","rsc":["$","$1","c",{"children":[null,["$","$L2",null,{"parallelRouterKey":"children","template":["$","$L3",null,{}]}]]}],"loading":null,"isPartial":false}
--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" }, // You can change "*" to "http://localhost:3001" for stricter security
+          { key: "Access-Control-Allow-Origin", value: "https://preuniversity.vercel.app" }, // You can change "*" to "http://localhost:3001" for stricter security
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
