c8257750b0fe3fe4aa40b1bfb997f21321fc5bd4
Mohamed-Hashim-km <hashimhx3@gmail.com>
2026-03-12 12:29:33 +0530

    file size

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb", // Added to allow the 14MB PDF upload
+    },
+  },
   async headers() {
     return [
       {
@@ -11,11 +16,14 @@ const nextConfig: NextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "https://preuniversity.vercel.app" }, // You can change "*" to "http://localhost:3001" for stricter security
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
     ];
-  }
+  },
 };
 
 export default nextConfig;
